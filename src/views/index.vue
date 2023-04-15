5ef6ae7c87043de6bd1ea81bb24e1e849c2b46bf
sysmaster <sysrj2010@163.com>
2023-04-15 23:57:07 +0800

    1、首页登录提示

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,23 +6,15 @@
       </el-col>
     </el-row>
 
-    <el-row :gutter="20">
-      <el-col :span="24">
-        <cms-site-stat></cms-site-stat>
-      </el-col>
-    </el-row>
   </div>
 </template>
 
 <script>
 import SysUserInfo from '@/views/system/dashboard/userInfo'
-import CmsSiteStat from '@/views/cms/dashboard/siteStat'
-
 export default {
   name: 'Index',
   components: {
     'user-info': SysUserInfo,
-    'cms-site-stat': CmsSiteStat
   },
   data() {
     return {
